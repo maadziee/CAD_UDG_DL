06d56761807d981aeb764ae4af724226201b2028
Madinabonu <93178061+maadziee@users.noreply.github.com>
2026-02-13 21:44:03 +0500

    Update exp_cv.py

--- exp_cv.py
+++ exp_cv.py
@@ -124,7 +124,7 @@ if __name__ == "__main__":
     CLASSES = ["bcc", "mel", "scc"] if DATASET == "Multiclass" else ["nevous", "others"]
 
     dagshub.init(
-        repo_owner="huytrnq", repo_name="Deep-Skin-Lesion-Classification", mlflow=True
+        repo_owner="madzie", repo_name="CAD_UDG_DL", mlflow=True
     )
     # Start MLflow tracking
     mlflow.start_run(run_name=f"Skin Lesion Classification - {DATASET}")
